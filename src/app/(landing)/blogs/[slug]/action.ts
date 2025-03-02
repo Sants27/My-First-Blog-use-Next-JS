d695855c956a101b1828ac59915c9bd6df4e7ae9
Sant <ignatius.varrel@gmail.com>
2025-03-02 16:05:00 +0700

    refactor: Rename actions.ts to action.ts and add submitBlog function to handle form submissions
    - Implement try/catch for error handling in API requests

---
+++ src/app/(landing)/blogs/[slug]/action.ts
@@ -0,0 +1,20 @@
+"use server";
+
+export async function testServerAction() {
+  console.log("ini action dari server yang dipanggil oleh client");
+}
+
+export async function submitBlog(
+    previousState: Record<string, string>,          // Menyimpan state sebelumnya dari form submission.
+    formData: unknown                               // formData : Data yang dikirimkan oleh form
+)   {
+    try {
+      const response = await fetch("pokeapi");      // Mengambil data dari API
+  
+      const data = await response.json();           // Mengubah response ke JSON
+  
+      return data;                                  // Mengembalikan hasil data
+    } catch (error) {                               // Menangani error jika terjadi
+      console.log(error);
+    }
+}
--- src/app/(landing)/blogs/[slug]/actions.ts
+++
@@ -1,5 +0,0 @@
-"use server";
-
-export async function testServerAction() {
-  console.log("ini action dari server yang dipanggil oleh client");
-}
