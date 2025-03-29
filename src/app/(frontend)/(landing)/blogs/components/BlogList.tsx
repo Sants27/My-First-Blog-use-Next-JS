359369068be87eb50d8e4305c735fba012741ed9
Sant <ignatius.varrel@gmail.com>
2025-03-29 10:03:02 +0700

    feat: integrate blog and logo fetching from Payload CMS
    - Fetched posts dynamically using Payload API in page.tsx
    - Passed posts data to BlogList
    - Updated BlogList to map posts from API instead of hardcoded data
    - Fetched logo from Global (General) and render to layout

--- src/app/(frontend)/(landing)/blogs/components/BlogList.tsx
+++ src/app/(frontend)/(landing)/blogs/components/BlogList.tsx
@@ -1,39 +1,45 @@
+import { Media, Post } from "@/payload-types";
 import Image from "next/image";
 import Link from "next/link";
+import { PaginatedDocs } from "payload";
 import React from "react";
 
-const posts = [
-  {
-    id: 1,
-    title: "Lorem Ipsum",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec odio vitae elit imperdiet ultricies.",
-    cover: "https://picsum.photos/seed/picsum/200/300",
-    slug: "lorem-ipsum",
-  },
-  {
-    id: 2,
-    title: "Dolor Sit Amet",
-    description:
-      "Dolor sit amet, consectetur adipiscing elit. Donec nec odio vitae elit imperdiet ultricies.",
-    cover: "https://picsum.photos/seed/picsum/200/300",
-    slug: "dolor-sit-amet",
-  },
-  {
-    id: 3,
-    title: "Consectetur Adipiscing",
-    description:
-      "Consectetur adipiscing elit. Donec nec odio vitae elit imperdiet ultricies.",
-    cover: "https://picsum.photos/seed/picsum/200/300",
-    slug: "consectetur-adipiscing",
-  },
-];
+// const posts = [
+//   {
+//     id: 1,
+//     title: "Lorem Ipsum",
+//     description:
+//       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec odio vitae elit imperdiet ultricies.",
+//     cover: "https://picsum.photos/seed/picsum/200/300",
+//     slug: "lorem-ipsum",
+//   },
+//   {
+//     id: 2,
+//     title: "Dolor Sit Amet",
+//     description:
+//       "Dolor sit amet, consectetur adipiscing elit. Donec nec odio vitae elit imperdiet ultricies.",
+//     cover: "https://picsum.photos/seed/picsum/200/300",
+//     slug: "dolor-sit-amet",
+//   },
+//   {
+//     id: 3,
+//     title: "Consectetur Adipiscing",
+//     description:
+//       "Consectetur adipiscing elit. Donec nec odio vitae elit imperdiet ultricies.",
+//     cover: "https://picsum.photos/seed/picsum/200/300",
+//     slug: "consectetur-adipiscing",
+//   },
+// ];
 
-export default function BlogList() {
+interface BlogListProps {
+  posts?: PaginatedDocs<Post>
+}
+
+export default function BlogList({ posts }: BlogListProps) {
   return (
     <section className="grid grid-cols-1 lg:grid-cols-3 gap-4 pb-8">
       <h2 className="text-4xl lg:col-span-3 font-bold px-8">Blogs</h2>
-      {posts.map((post) => {
+      {posts?.docs.map((post) => {
         return (
           <article
             key={post.id}
@@ -41,13 +47,13 @@ export default function BlogList() {
           >
             <Image
               className="w-full object-cover h-56"
-              src={post.cover ?? ""}
-              alt={post.title}
+              src={(post.cover as Media).url ?? ""}
+              alt={(post.cover as Media).alt}
               width={300}
               height={300}
             />
             <time className="text-zinc-500 p-0 h-fit border-l-2 border-zinc-500 pl-4 block">
-              2 Januari 2025
+              {post.createdAt}
             </time>
             <h3 className="font-bold">{post.title}</h3>
             <p className="text-zinc-400 text-sm">{post.description}</p>
--- src/app/(frontend)/(landing)/page.tsx
+++ src/app/(frontend)/(landing)/page.tsx
@@ -1,16 +1,30 @@
 import Image from "next/image";
-import Logo from "@/assets/images/logo-circle.png";
+// import Logo from "@/assets/images/logo-circle.png";
 import InstagramIcon from "@/assets/icons/instagram";
 import LinkedinIcon from "@/assets/icons/linkedin";
 import FacebookIcon from "@/assets/icons/facebook";
 import GithubIcon from "@/assets/icons/github";
 import BlogList from "./blogs/components/BlogList";
-import BlogForm from "./blogs/components/BlogForm";
-export default function Home() {
+// import BlogForm from "./blogs/components/BlogForm";
+import { getPayload } from "payload";
+import payloadConfig from "@payload-config";
+import { Media } from "@/payload-types";
+
+export default async function Home() {
+  const general = (await getPayload({config: payloadConfig})).findGlobal({
+    slug: "general"
+  });
+
+  const Posts = (await getPayload({config: payloadConfig})).find({
+    collection: "posts"
+  })
+
+  const logo = (await general).logo as Media;
+  // console.log("check : ", {logo, general});
   return (
     <>
       <section className="space-y-8 p-8 pt-12">
-        <Image className="w-32" src={Logo} alt="logo blog cat" width={500} height={500} />
+        <Image className="w-32" src={logo.url ?? ""} alt={logo.alt} width={500} height={500} />
         <h1 className="text-6xl font-bold max-w-4xl">Software Enginer, Designer, and Content Creator</h1>
         <p className="text-zinc-400 max-w-2xl">Lorem, ipsum dolor sit amet consectetur
         adipisicing elit. Maiores recusandae officiis totam corrupti obcaecati autem cumque
@@ -23,8 +37,8 @@ export default function Home() {
           <FacebookIcon />
         </div>
       </section>
-      <BlogList />
-      <BlogForm />
+      <BlogList posts={(await Posts)}/>
+      {/* <BlogForm /> */}
     </>
   );
 }
